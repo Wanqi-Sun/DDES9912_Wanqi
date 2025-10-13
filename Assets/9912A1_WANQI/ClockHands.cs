9753874c7a4a9674d22a090f7b8b17853b532493
Wanqi-Sun <z5618328@ad.unsw.edu.au>
2025-10-13 22:36:34 +1100

    6

--- Assets/9912A1_WANQI/ClockHands.cs
+++ Assets/9912A1_WANQI/ClockHands.cs
@@ -6,17 +6,22 @@ public class ClockHands : MonoBehaviour
     public Transform pivotMinute;
     public Transform pivotSecond;
 
+    void Awake()
+    {
+        this.enabled = false;  // 提前禁用，防止 Update 提前运行
+    }
+
     void Update()
     {
         var t = System.DateTime.Now;
 
-        float zSec   = -t.Second * 6f;                               // 360/60
-        float zMin   = -(t.Minute * 6f + t.Second * 0.1f);           // 每秒多0.1°
-        float zHour  = -((t.Hour % 12) * 30f + t.Minute * 0.5f);     // 360/12 + 分钟影响
+        float zSec = -t.Second * 6f;
+        float zMin = -(t.Minute * 6f + t.Second * 0.1f);
+        float zHour = -((t.Hour % 12) * 30f + t.Minute * 0.5f);
 
         pivotSecond.localRotation = Quaternion.Euler(0, 0, zSec);
         pivotMinute.localRotation = Quaternion.Euler(0, 0, zMin);
-        pivotHour  .localRotation = Quaternion.Euler(0, 0, zHour);
+        pivotHour.localRotation = Quaternion.Euler(0, 0, zHour);
     }
 }
 
